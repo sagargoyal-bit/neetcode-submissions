43aa2df959be07710f561842ea5216e81e1438b6
Sagar.G <54657695+sagargoyal-bit@users.noreply.github.com>
2026-09-09 15:22:19 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.go
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * type Interval struct {
+ *    start int
+ *    end   int
+ * }
+ */
+
+func canAttendMeetings(intervals []Interval) bool {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].start < intervals[j].end
+	})
+	if len(intervals) == 0 {
+			return true
+	}
+
+for i := 1; i < len(intervals); i++ {
+	prevEnd := intervals[i-1]
+	curr:=intervals[i]
+    if curr.start < prevEnd.end {
+        return false
+    }
+}
+return true
+}
